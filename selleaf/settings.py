f93532e54be05ce105cd5157c37e6e55eea13705
dianakang <dianakang0123@gmail.com>
2024-03-20 14:59:39 +0900

    selleaf init

--- selleaf/settings.py
+++ selleaf/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-xrbh@fs#!56dnm^+1ld6l5n5t7n)1d@6t(@r3a&2!_x1j8r*$r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '3.36.106.41']
+ALLOWED_HOSTS = ['3.36.78.185' ,'127.0.0.1', 'localhost']
 
 # Application definition
 
@@ -166,6 +166,8 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # 파일 접근 시
 MEDIA_URL = '/upload/'
